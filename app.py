ace2fb418cef4de0db465338856ae45f49a0f405
Sahampath <92423401+Sahampath@users.noreply.github.com>
2026-08-23 23:55:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1522,7 +1522,7 @@ if __name__ == "__main__":
     if sys.platform == 'win32':
         try:
             import ctypes
-            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("EroMeDownloader.RJ.1.0.7")
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("EroMeDownloader.RJ.1.0.8")
         except:
             pass
 
